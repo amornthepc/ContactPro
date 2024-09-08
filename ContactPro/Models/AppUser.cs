16e3f854ce8bf6a4d28cdd353531f4fed81f0bb0
Amornthep Chuenchom <amornthep.chuen@gmail.com>
2024-09-08 16:29:43 +0700

    Finished part 11 - Template Navigation Bar

---
+++ ContactPro/Models/AppUser.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactPro.Models
+{
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [DisplayName("First Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1}", MinimumLength = 2)]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [DisplayName("Last Name")]
+        [StringLength(50, ErrorMessage = "The {0} must be at least {2} and a max {1}", MinimumLength = 2)]
+        public string? LastName { get; set; }
+
+        [NotMapped]
+        public string? FullName => $"{FirstName} {LastName}";
+
+        // Navigation Properties
+        public virtual ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+        public virtual ICollection<Contact> Contacts { get; set; } = new HashSet<Contact>();
+    }
+}
---
+++ ContactPro/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContactPro.Models
+{
+    public class Category
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [ForeignKey("AppUser")]
+        [Required]
+        public string? AppUserId { get; set; }
+
+        [Required]
+        [Display(Name = "Category Name")]
+        public string? Name { get; set; }
+
+        // Virtuals - Navigation property
+        public virtual AppUser? AppUser { get; set; }
+        public virtual ICollection<Contact>? Contacts { get; set; } = new HashSet<Contact>();
+    }
+}
